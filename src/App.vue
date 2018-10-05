cc9f8c9163f909136bd04ab50333b894a4e8c56a
David M <mullaned@gmail.com>
2018-10-05 14:03:42 +0100

    central store issue with multipe results

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
                 <app-result></app-result>
+                <br><br>
+                <app-another-result></app-another-result>
                 <hr>
                 <app-counter></app-counter>
             </div>
@@ -14,11 +16,13 @@
 <script>
     import Counter from './components/Counter.vue';
     import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
 
     export default {
         components: {
             appCounter: Counter,
             appResult: Result,
+            appAnotherResult: AnotherResult,
         }
     }
 </script>
